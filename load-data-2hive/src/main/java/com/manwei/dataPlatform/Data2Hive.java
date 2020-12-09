f34dea12b62959cb876c8f08938f7f1a4ef2ceeb
程飞 <chengfei@newland.com.cn>
2020-12-09 15:48:06 +0800

    数据成功load到hive

--- load-data-2hive/src/main/java/com/manwei/dataPlatform/Data2Hive.java
+++ load-data-2hive/src/main/java/com/manwei/dataPlatform/Data2Hive.java
@@ -5,8 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Properties;
 
 /**
  * @ClassName: Data2Hive
@@ -17,14 +19,21 @@ import java.time.LocalDate;
 public class Data2Hive {
     static Logger logger = LoggerFactory.getLogger(Data2Hive.class);
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
+
+        InputStream resourceAsStream = Data2Hive.class.getResourceAsStream("/db.properties");
+        Properties properties = new Properties();
+        properties.load(resourceAsStream);
+
         //链接hive
         HiveJDBC hiveJDBC = new HiveJDBC();
-        //        String sqlStr = "load data local inpath '/root/data/log/" + LocalDate.now().plusDays(-1).toString() +".log' overwrite into table ods_cxy.action_data " +
-//                "partition(department = 'cxy',datetime = '"+ LocalDate.now().plusDays(-1).toString() + "')";
+
+        String sqlStr = "load data local inpath '"+ properties.getProperty("dataPath") + LocalDate.now().plusDays(-1).toString() +".log' overwrite into table ods_cxy.action_data " +
+                "partition(department = 'cxy',datetime = '"+ LocalDate.now().plusDays(-1).toString() + "')";
 
         //导入hdfs上的文件
-        String sqlStr = "load data inpath '/2020-12-07.log' overwrite into table ods_cxy.action_data " +
-                "partition(department = 'cxy',datetime = '"+ LocalDate.now().plusDays(-1).toString() + "')";
+//        String sqlStr = "load data inpath '/user/hadoop/2020-12-07.log' overwrite into table ods_cxy.action_data " +
+//                "partition(department = 'cxy',datetime = '"+ LocalDate.now().plusDays(-1).toString() + "')";
+
         hiveJDBC.executeSQL(sqlStr);
         logger.info("数据导入成功");
     }
